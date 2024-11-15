4563adb0c13469cb7901e0639ccd25ce4a2b0a1a
przemjan <przemjanisz2@gmail.com>
2024-11-15 07:58:48 +0100

    Initial commit

---
+++ src/Zmienne.java
@@ -0,0 +1,29 @@
+public class Zmienne {
+    public static void main(String[] args) {
+
+        int liczba;
+        liczba = 13;
+
+        double liczba2 = 17.7;
+
+        System.out.println("Moja zmienna: " + liczba);
+
+        int days = 5;
+        int hours = 8;
+        int weeks = 52;
+
+
+        //int x1 = 5 * 8 * 52;
+
+        int workHoursPerYear = days % hours * weeks;
+        System.out.println("Wynik: " + workHoursPerYear);
+
+
+        char znak = '#';
+
+        
+
+
+
+    }
+}
